38333c89f05596f520d8233c3de0843fe84ab474
Ashik Basheer <ashikdnd@gmail.com>
2022-04-24 10:45:44 +0530

    Completed chatroom

--- app.js
+++ app.js
@@ -9,19 +9,45 @@ const io = new Server({
 users = [];
 
 io.on("connection", (socket) => {
-  console.log(socket.id)
 
   io.emit('userList', users);
+  io.to(socket.id).emit('myId', {
+    socketId: socket.id
+  });
+
 
   socket.on('updateName', (name) => {
     console.log('update name')
     users.unshift({
       name: name,
-      sid: socket.id
+      sid: socket.id,
+      msg: '',
+      lastSeen: ''
     });
     socket.user_name = name;
     io.emit('updateUsers', users);
   });
+
+  socket.on('sendMessage', (msg) => {
+    const user = users.filter(f => {
+      return f.sid == socket.id;
+    });
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].sid == socket.id) {
+        users[i].msg = msg;
+        users[i].time = now();
+      }
+    }
+    io.emit('updateUsers', users);
+
+    if (user.length) {
+      io.emit('incomingMsg', {
+        msg: msg,
+        username: user[0].name,
+        socket: socket.id
+      });
+    }
+  })
 
   socket.on('disconnect', () => {
     const u = users.filter((f) => {
@@ -32,5 +58,20 @@ io.on("connection", (socket) => {
   });
 
 });
+
+function now() {
+  // 👇️ from CURRENT DATE
+  const now = new Date();
+  const current = now.getHours() + ':' + now.getMinutes();
+
+// 👇️ With PM / AM
+  const withPmAm = now.toLocaleTimeString('en-US', {
+    // en-US can be set to 'default' to use user's browser settings
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  return withPmAm;
+}
 
 io.listen(3000);
